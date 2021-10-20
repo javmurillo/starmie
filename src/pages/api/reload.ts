d8ba03f91d121ff8def5a800fec39fba6d271952
Javier Murillo <jmurillo93@gmail.com>
2021-10-21 01:52:14 +0200

    test

--- src/pages/api/reload.ts
+++ src/pages/api/reload.ts
@@ -5,16 +5,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { name } = req.query
-  const pokemon = await PokemonsService.getPokemon(name)
+  // const { name } = req.query
+  // const pokemon = await PokemonsService.getPokemon(name)
 
-  const [newStrongAgainst, newWeakAgainst] = await Promise.all([
-    PokemonsService.getStrongAgainst(pokemon),
-    PokemonsService.getWeakAgainst(pokemon),
-  ])
+  // const [newStrongAgainst, newWeakAgainst] = await Promise.all([
+  //   PokemonsService.getStrongAgainst(pokemon),
+  //   PokemonsService.getWeakAgainst(pokemon),
+  // ])
 
-  res.status(200).json({
-    newStrongAgainst,
-    newWeakAgainst,
-  })
+  res.status(200).json({})
 }
